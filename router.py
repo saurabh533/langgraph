7c4865c1e12b5a948e0aae84572b78a1602fd335
SAURABH RAOKHANDE <124454892+saurabh533@users.noreply.github.com>
2026-04-05 11:23:24 -0400

    Add files via upload

---
+++ router.py
@@ -0,0 +1,35 @@
+from typing import Literal
+from pydantic import BaseModel
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+
+
+
+#-------------------------------------------
+# SECURELY LOAD YOUR GROQ API KEY           
+#--------------------------------------------
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0)
+# ─────────────────────────────────────────
+# 2. ROUTER
+# ─────────────────────────────────────────
+class RouteDecision(BaseModel):
+    destination: Literal["predictive_analytics", "general_query"]
+
+#llm = ChatOpenAI(model="gpt-4o", temperature=0)
+
+router_chain = ChatPromptTemplate.from_messages([
+    ("system", """Route the query:
+- 'predictive_analytics': forecasting, predictions, trends,"Relative_Compactness","Surface_Area","Wall_Area","Roof_Area","Overall_Height","Orientation","Glazing_Area","Glazing_Area_Distribution"
+- 'energy_domain_query': factual questions, explanations, domain knowledge, general energy information"""),
+    ("human", "{query}")
+]) | llm.with_structured_output(RouteDecision)
+
+def query_router_node(state: dict) -> dict:                         ############## first node 
+    decision = router_chain.invoke({"query": state["query"]})
+    print(f"[ROUTER] Route decision: {decision.destination}")
+    return {"route": decision.destination}
+
+def route_decision(state: dict) -> str:
+    return state["route"]
